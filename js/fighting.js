3657238bc1f62bd45b1d2c693a262b67ba3c465f
Ettenytten <honeygeeh27@gmail.com>
2022-04-22 13:10:19 +0200

    commit

--- js/fighting.js
+++ js/fighting.js
@@ -1,6 +1,6 @@
 const URL = "https://nenorvalls.no/flower-power/gamehub/wp-json/wc/v3/products/"
 
-const key = "?consumer_key=ck_6131069f7cf8fe34078860b7f32de680257422f1&consumer_secret=cs_81c77c45ce7a85e90c9f21bc157818964c403cc3"
+const key = "?consumer_key=ck_6131069f7cf8fe34078860b7f32de680257422f1&consumer_secret=cs_81c77c45ce7a85e90c9f21bc157818964c403cc&per_page=30"
 
 const gamesContainer = document.querySelector(".results")
 
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const gamesContainer = document.querySelector(".results");
 
-const gamesURL = "https://nenorvalls.no/flower-power/gamehub/wp-json/wc/store/v1/products?consumer_key=ck_8b7263146b06be0805c5e9b1973c98fb03a3f819&consumer_secret=cs_0477da413d9d64e0957e4e3acfd8c68effa3dbfc";
+const gamesURL = "https://nenorvalls.no/flower-power/gamehub/wp-json/wc/store/v1/products?consumer_key=ck_6131069f7cf8fe34078860b7f32de680257422f1&consumer_secret=cs_81c77c45ce7a85e90c9f21bc157818964c403cc3";
 
 const corsFix = "https://noroffcors.herokuapp.com/"
 
